eddbbb66fb1f7e07628b58d07ae9aea1812bf218
Carlos Chinchilla <calufa@gmail.com>
2020-05-01 13:24:38 -0600

    adjust job_manager to exit after timer

--- jobs_manager.py
+++ jobs_manager.py
@@ -40,32 +40,28 @@ def prepare_job(output_dir_path, story_id, job_name):
 
 
 def subscribe(job):
-	# Due to a strange bug, it seems (not certain), that the subscriber 
-	# "drops" connection to pubsub.
-	# Adding an infinite loop helps circumvent the issue.
-	while True:
-		def callback(message):
-			story_id = message.data.decode('utf-8') # binary to utf-8 string
+	def callback(message):
+		story_id = message.data.decode('utf-8') # binary to utf-8 string
 
-			if next_job:
-				launch_job(story_id, next_job)
-			else:
-				print('story id "{}" completed successfully'.format(story_id))
+		if next_job:
+			launch_job(story_id, next_job)
+		else:
+			print('story id "{}" completed successfully'.format(story_id))
 
-			message.ack()
+		message.ack()
 
-		subscription = job['subscription']
-		next_job = job.get('next-job')
-		print('subscribed to:', subscription)
+	subscription = job['subscription']
+	next_job = job.get('next-job')
+	print('subscribed to:', subscription)
 
-		subscriber = pubsub_v1.SubscriberClient()
-		subscription_path = subscriber.subscription_path(COMPUTE_PROJECT_NAME, subscription)
-		subscriber.subscribe(subscription_path, callback=callback)
+	subscriber = pubsub_v1.SubscriberClient()
+	subscription_path = subscriber.subscription_path(COMPUTE_PROJECT_NAME, subscription)
+	subscriber.subscribe(subscription_path, callback=callback)
 
-		time.sleep(30)
+	time.sleep(30)
 
-		subscriber.close()
-		print('closing subscription to:', subscription)
+	subscriber.close()
+	print('closing subscription to:', subscription)
 
 
 if __name__ == '__main__':
@@ -73,6 +69,12 @@ if __name__ == '__main__':
 		thread = threading.Thread(target=subscribe, args=(job,))
 		thread.start()
 
-	# Prevents app from exiting
-	while True:
-		time.sleep(60)
+	# Prevents app from exiting.
+	# while True:
+	# 	time.sleep(60)
+
+	# Due to a strange bug, it seems (not certain), that the subscriber 
+	# "drops" connection to pubsub.
+	# Adding a time sleep before existing allows for this script to
+	# for a period of time before k8s reschedules the pod.
+	time.sleep(60 * 5)
